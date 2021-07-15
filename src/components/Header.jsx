6db79e696596f396e1b42648ce23cd6bcfe42819
your username <vyasavula6@gmail.com>
2021-07-15 12:13:17 +0530

    Intial Commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,3 +9,41 @@ function Header() {
 }
 
 export default Header;
+
+
+import React from "react";
+import "./App.css";
+import Todo from "./components/todo";
+function App() {
+  return (
+    <div>
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
+
+
+
+https://play.google.com/store/apps/details?id=com.fastmarket.fm
+
+
+ <nav>
+          {/* <img className="logo" src={logo} alt="Logo"></img> */}
+          <img className="logo" src={logocontent} alt="Logo"></img>
+          <ul>
+            <li>
+              <a href="#about-me">About</a>
+            </li>
+            <li>
+              <a href="#projects">Services</a>
+            </li>
+            <li>
+              <a href="#contact-me">Contact</a>
+            </li>
+            <button className="hamburger" id="hamburger">
+              <i className="fas fa-bars"></i>
+            </button>
+          </ul>
+        </nav>
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,20 +1,74 @@
 import React from "react";
+import "./expenses.css";
+import ExpenseItem from "./ExpenseItem";
+import ExpenseFilter from "./NewExpenses/ExpenseFilter";
 
-function Note(props) {
-
-
-function handleClick(){
-props.onDelete(props.id);
-
-}
+const Expenses = (props) => {
+  const saveYearDataHandler = (enteredYear) => {
+    console.log("filter");
+    console.log(enteredYear);
+  };
 
   return (
-    <div className="note">
-      <h1>{props.title}</h1>
-      <p>{props.content}</p>
-      <button onClick={handleClick}>DELETE</button>
+    <div>
+      <div className="expenses">
+        <ExpenseFilter onChangedFilter={saveYearDataHandler} />
+        <ExpenseItem
+          title={props.expenses[0].title}
+          amount={props.expenses[0].amount}
+          date={props.expenses[0].date}
+        />
+        <ExpenseItem
+          title={props.expenses[1].title}
+          amount={props.expenses[1].amount}
+          date={props.expenses[1].date}
+        />
+        <ExpenseItem
+          title={props.expenses[2].title}
+          amount={props.expenses[2].amount}
+          date={props.expenses[2].date}
+        />
+        <ExpenseItem
+          title={props.expenses[3].title}
+          amount={props.expenses[3].amount}
+          date={props.expenses[3].date}
+        />
+      </div>
     </div>
   );
-}
+};
 
-export default Note;
+export default Expenses;
+////////////////////////////////////////////
+
+
+import React, { useState } from "react";
+import "./ExpenseFilter.css";
+
+const ExpenseFilter = (props) => {
+  const [enteredYear, setEnteredYear] = useState("2020");
+
+  const yearChangedHandler = (event) => {
+    //event.preventDefault();
+    setEnteredYear(event.target.value);
+
+    props.onChangedFilter(enteredYear);
+  };
+
+  return (
+    <div className="expenses-filter">
+      <div className="expenses-filter__control">
+        <label>Filter by year</label>
+        <select onChange={yearChangedHandler} value={enteredYear}>
+          <option value="2022">2022</option>
+          <option value="2021">2021</option>
+          <option value="2020">2020</option>
+          <option value="2019">2019</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseFilter;
+
